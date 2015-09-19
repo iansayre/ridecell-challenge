87f217e9fbb0c4c43b50028c85e52c7e5c880629
iansayre <me@iansayre.com>
2015-09-19 07:55:36 -0700

    added some bootstrap styles. show and hide success and error messages

--- client/restaurants/controllers/restaurantMenu.js
+++ client/restaurants/controllers/restaurantMenu.js
@@ -1,12 +1,18 @@
 angular.module('foodApp').controller('MenuCtrl', ['$scope', '$stateParams', '$meteor', '$rootScope', 'OrderService', function($scope, $stateParams, $meteor, $rootScope, OrderService){
 	$scope.restaurant = $meteor.object(Restaurants, $stateParams.restaurantId, false);
 
-	var cart = [];
+	var cart = [],
+		errorAlert = document.getElementsByClassName('alert-danger')[0],
+		successAlert = document.getElementsByClassName('alert-success')[0];
 
 	$scope.addToCart = function() {
+		errorAlert.style.display = 'none';
+		successAlert.style.display = 'none';
+
 		if( $rootScope.currentUser !== null ){
 
 			var cartItem = {},
+				cartBadge = document.getElementsByClassName('cart-badge')[0],
 				item = this.food.name,
 				qty = document.getElementById(item + '-qty').value,
 				price = this.food.price;
@@ -17,17 +23,26 @@ angular.module('foodApp').controller('MenuCtrl', ['$scope', '$stateParams', '$me
 				cartItem.qty = qty;
 				cartItem.price = Number(price * qty);
 				cart.push(cartItem);
+
+				cartBadge.innerHTML = Number(cartBadge.innerHTML) + Number(qty);
+				successAlert.innerHTML = 'You placed this item in the cart';
+				successAlert.style.display = 'block';
 			}
 			else {
-				alert("You can't order zero foods");
+				errorAlert.innerHTML = "You can't order zero foods";
+				errorAlert.style.display = 'block';
 			}
 		}
 		else {
-			alert('You must be logged in to place an order');
+			 errorAlert.innerHTML = 'You must be logged in to place an order';
+			errorAlert.style.display = 'block';
 		}
 	};
 
 	$scope.placeOrder = function() {
+		errorAlert.style.display = 'none';
+		successAlert.style.display = 'none';
+
 		if( $rootScope.currentUser !== null ) {
 			var pickupTime = document.getElementById('timeSelect').value;
 
@@ -38,9 +53,13 @@ angular.module('foodApp').controller('MenuCtrl', ['$scope', '$stateParams', '$me
 				console.log(cart);
 				console.log('pickup time: ' + pickupTime);
 				console.log('currentUser! ' + $rootScope.currentUser._id);
+
+				successAlert.innerHTML = 'Your order has been placed.';
+				successAlert.style.display = 'block';
 			}
 			else {
-				alert('You must choose a pickup time before submitting an order.');
+				errorAlert.innerHTML = 'You must choose a pickup time before submitting an order.';
+				errorAlert.style.display = 'block';
 			}
 		}
 	};
