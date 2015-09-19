958d6be7e995a42621f4f33b38866fc19b8c1750
iansayre <me@iansayre.com>
2015-09-18 22:18:34 -0700

    can add a food item to cart now

--- client/restaurants/controllers/restaurantMenu.js
+++ client/restaurants/controllers/restaurantMenu.js
@@ -1,5 +1,29 @@
-  angular.module('foodApp').controller('MenuCtrl', ['$scope', '$stateParams', '$meteor', function($scope, $stateParams, $meteor){
-    $scope.restaurant = $meteor.object(Restaurants, $stateParams.restaurantId, false);
+angular.module('foodApp').controller('MenuCtrl', ['$scope', '$stateParams', '$meteor', function($scope, $stateParams, $meteor){
+  $scope.restaurant = $meteor.object(Restaurants, $stateParams.restaurantId, false);
 
-    $scope.cart = $meteor.collection(Cart);
-  }]);
+  var cart = [];
+
+  $scope.addToCart = function() {
+    var cartItem = {},
+        item = this.food.name,
+        qty = document.getElementById(item + '-qty').value,
+        price = this.food.price;
+
+
+        if( qty > 0 ) {
+          cartItem.item = item;
+          cartItem.qty = qty;
+          cartItem.price = Number(price);
+          cart.push(cartItem);
+        }
+        else {
+          alert("You can't order zero foods");
+        }
+
+        console.log('ITEM: ' + item);
+        console.log('QTY ' + qty);
+        console.log('price ' + price);
+
+        console.log(cart);
+  };
+}]);
--- model/cart.js
+++ model/cart.js
@@ -1,13 +1,3 @@
-Cart = new Mongo.Collection('cart');
 
-Cart.allow({
-	insert: function(userId, food) {
-		// do stuff
-	},
-	update: function(userId, food) {
-		// do stuff
-	},
-	remove: function(userId, food) {
-		// do stuff
-	}
-});
+
+
